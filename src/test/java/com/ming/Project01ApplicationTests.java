652977d8c66b68d76d428bd67f5d53fdc4457deb
6uiwj <oterges00@gmail.com>
2024-07-27 02:08:47 +0900

    240726 - 질문생성 테스트 실행

--- src/test/java/com/ming/Project01ApplicationTests.java
+++ src/test/java/com/ming/Project01ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.ming;
 
+import com.ming.question.Question;
+import com.ming.question.QuestionRepository;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Project01ApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb 제목");
+		q1.setContent("sbb 내용");
+		q1.setCreateDate(LocalDateTime.now());
+		 this.questionRepository.save(q1);
+
+	 	Question q2 = new Question();
+		 q2.setSubject("sbb 제목2");
+		 q2.setContent("sbb 내용2");
+		 q2.setCreateDate(LocalDateTime.now());
+		 this.questionRepository.save(q2);
+
 	}
+
+
+
+
 
 }
