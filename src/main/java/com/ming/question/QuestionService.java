51f68b5afa1aa545427508da03069f16195dca60
6uiwj <oterges00@gmail.com>
2024-08-12 23:00:34 +0900

    240812 - 게시글 최신순 정렬

--- src/main/java/com/ming/question/QuestionService.java
+++ src/main/java/com/ming/question/QuestionService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,11 +18,23 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+    /**
+     * 게시글 목록 출력
+     * @param page
+     * @return
+     */
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page,10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 
+    /**
+     * id값으로 질문데이터 조회
+     * @param id
+     * @return
+     */
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
         if(question.isPresent()) {
