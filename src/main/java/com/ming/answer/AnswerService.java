9294471501ea4534e58a6a7cb553f1916550c41f
6uiwj <oterges00@gmail.com>
2024-08-26 23:04:20 +0900

    240826 - 답변 수정

--- src/main/java/com/ming/answer/AnswerService.java
+++ src/main/java/com/ming/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.ming.answer;
 
+import com.ming.DataNotFoundException;
 import com.ming.question.Question;
 import com.ming.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +25,32 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    /**
+     * 답변 조회
+     * @param id
+     * @return
+     */
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+
+        }
+    }
+
+    /**
+     * 답변 수정
+     * @param answer
+     * @param content
+     */
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
